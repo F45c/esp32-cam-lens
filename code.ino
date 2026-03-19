95b15149f01a7ac86738415cefdb0615431f0c87
F45c <269475773+F45c@users.noreply.github.com>
2026-03-19 15:55:52 +0100

    added esp32 firmware

---
+++ code.ino
@@ -0,0 +1,116 @@
+#include "esp_camera.h"
+#include <WiFi.h>
+
+#define CAMERA_MODEL_ESP32S3_EYE // Has PSRAM
+
+#include "camera_pins.h"
+
+const char *ssid = "XXXXXXXXXXXXXXXXXXXXXXXXXX";
+const char *password = "XXXXXXXXXXXXXXXXXXXXXXXXXX";
+
+void startCameraServer();
+void setupLedFlash(int pin);
+
+#define servo1 15
+#define servo2 16
+
+bool output = 0;
+long long lastMicros = 0;
+long long lastUpdate = 0;
+
+void turnTo(int pin, int degree) {
+  degree = degree < 0 ? 0 : (degree > 180) ? 180 : degree;
+  int t1 = map(degree, 0, 180, 1000, 2000);
+  if(lastMicros + (output ? t1 : 20000 - t1) < micros()) {
+    digitalWrite(pin, output);
+    output = !output;
+  }
+}
+
+void setup() {
+  pinMode(servo1, OUTPUT);
+  pinMode(servo2, OUTPUT);
+  turnTo(servo1, 165);
+  turnTo(servo2, 30);
+  
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println();
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sccb_sda = SIOD_GPIO_NUM;
+  config.pin_sccb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.frame_size = FRAMESIZE_VGA;
+  config.pixel_format = PIXFORMAT_JPEG;
+  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+  config.fb_location = CAMERA_FB_IN_PSRAM;
+  config.jpeg_quality = 12;
+  config.fb_count = 1;
+  if (config.pixel_format == PIXFORMAT_JPEG) {
+    if (psramFound()) {
+      config.jpeg_quality = 10;
+      config.fb_count = 2;
+      config.grab_mode = CAMERA_GRAB_LATEST;
+    } else {
+      config.frame_size = FRAMESIZE_SVGA;
+      config.fb_location = CAMERA_FB_IN_DRAM;
+    }
+  } else {
+    config.frame_size = FRAMESIZE_240X240;
+#if CONFIG_IDF_TARGET_ESP32S3
+    config.fb_count = 2;
+#endif
+  }
+
+#if defined(CAMERA_MODEL_ESP_EYE)
+  pinMode(13, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+#endif
+
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  sensor_t *s = esp_camera_sensor_get();
+  s->set_vflip(s, 1);
+
+  WiFi.begin(ssid, password);
+  WiFi.setSleep(false);
+
+  Serial.print("WiFi connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  startCameraServer();
+
+  Serial.print("Camera Ready! Use 'http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("' to connect");
+}
+
+void loop() {
+  delay(10000);
+}
